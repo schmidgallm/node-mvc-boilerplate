b80b51285cceccdd0c7cceec7f12f85c8421a1d2
schmidgallm <schmidgallm@outlook.com>
2020-05-16 22:14:06 -0700

    update profile model with user who created product and product post route in product route

--- models/Product.js
+++ models/Product.js
@@ -2,9 +2,26 @@
 const mongoose = require('mongoose');
 
 const ProductSchema = new mongoose.Schema({
-  item_id: String,
-  item_description: String,
-  price: Number
+  item_id: {
+    type: String,
+    required: true
+  },
+  item_description: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  created_by: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  creation_date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Product = mongoose.model('product', ProductSchema);
--- routes/api/product.js
+++ routes/api/product.js
@@ -33,16 +33,29 @@ router.get('/', async (req, res) => {
 // @desc    POST a product
 // @access  PRIVATE
 router.post('/', auth, async (req, res) => {
-  try {
-    // search for all product
-    const product = await Product.find({}).sort({ item_id: 1 });
+  // desctrucutre request body
+  const { item_id, description, price } = req.body;
 
-    // check if no product exists
-    if (!product) {
-      return res.status(404).json({ msg: 'No product found' });
+  try {
+    // check if product exists
+    let product = await Product.findOne({ item_id: item_id });
+
+    if (product) {
+      return res.status(400).json({ msg: 'Product already exists' });
     }
 
-    // return all products
+    // if product does not exist then create new one
+    product = new Product({
+      item_id,
+      description,
+      price,
+      created_by: req.user.id
+    });
+
+    // save to db
+    await product.save();
+
+    // return response
     return res.status(200).json(product);
   } catch (err) {
     console.warn(err.message);
