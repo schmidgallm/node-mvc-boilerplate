992faeab672956227775d308190bad777e0a1d91
schmidgallm <schmidgallm@outlook.com>
2020-05-23 11:50:25 -0700

    ui revision

--- public/src/App.js
+++ public/src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="header">
+        <h1>Node Full Stack MVC Boilerplate</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Application utlizing node in back, express, API crud, custom auth
+          middleware, MongoDb, React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
